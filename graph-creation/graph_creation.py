99638ef0e29d13e794bbbb9565de5a4048bbfc48
Sriram Baskaran <sbaskara@usc.edu>
2018-04-30 19:38:50 -0700

    Naming fixes

--- graph-creation/graph_creation.py
+++ graph-creation/graph_creation.py
@@ -8,7 +8,7 @@ conf = SparkConf().setAppName("Create graph")
 sc   = SparkContext(conf=conf)
 
 inputFile = "s3a://graph-cd-bucket/ratings.csv"
-outputFile = "s3a://graph-cd-bucket/edges.txt"
+outputFile = "s3a://graph-cd-bucket/edges"
 THRESHOLD = 4.0
 
 # Creating a RDD of (movie_id, user_id) for all ratings > THRESHOLD
