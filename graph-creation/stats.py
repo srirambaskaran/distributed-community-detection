0708875c97193ff8958339f9fba1c0dfbc77cebb
Sriram Baskaran <sbaskara@usc.edu>
2018-05-03 12:28:01 -0700

    Gather basic status about the graph

--- graph-creation/stats.py
+++ graph-creation/stats.py
@@ -5,13 +5,13 @@ import sys
 conf = SparkConf().setAppName("Get stats about graph")
 sc   = SparkContext(conf=conf)
 
-if len(sys.argv) < 1:
+if len(sys.argv) < 2:
 	print "Usage: <input-file:string>"
 	raise SystemExit
 
 inputFolder = sys.argv[1]
 
-lines = sc.textFile("s3a://graph-cd-bucket/edges-gt-4_weighted.csv/part-*")
+lines = sc.textFile(inputFolder)
 
 print str(lines.count())
 
